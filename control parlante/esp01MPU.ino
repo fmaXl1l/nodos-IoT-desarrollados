da56f1825903441483d7277663fdbfbcb7a8da01
fmaXl1l <42846708+fmaXl1l@users.noreply.github.com>
2023-01-12 21:11:07 -0500

    Update esp01MPU.ino

--- control parlante/esp01MPU.ino
+++ control parlante/esp01MPU.ino
@@ -15,8 +15,8 @@
 WiFiClient esp01MPUClient;
 PubSubClient mqttClient(esp01MPUClient);
 
-const char* ssid = "ALDRINCNT";
-const char* password = "0400916805";
+const char* ssid = "SSID";
+const char* password = "PASSWORD";
 const char* server = "192.168.30.50";
 const int port = 1883;
 const char *MQTT_CLIENT_NAME = "ESP01_MPU";
